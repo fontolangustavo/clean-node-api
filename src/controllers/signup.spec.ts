fb2a04f2b7b1e91fa70be92889a305a1fe8cbf8c
Fontolan <fontolangustavo@gmail.com>
2023-09-05 16:01:53 -0300

    test: ensure SignUpControler calls EmailValidator with correct email

--- src/controllers/signup.spec.ts
+++ src/controllers/signup.spec.ts
@@ -107,4 +107,22 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new InvalidParamError('email'))
   })
+
+  it('should call EmailValidator with correct email', () => {
+    const { sut, emailValidatorStub } = makeSut()
+
+    const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+
+    const httpRequest = {
+      body: {
+        email: 'valid_email@email.com',
+        name: 'any_name',
+        password: 'any_password',
+        password_confirmation: 'any_password'
+      }
+    }
+
+    sut.handle(httpRequest)
+    expect(isValidSpy).toHaveBeenCalledWith('valid_email@email.com')
+  })
 })
