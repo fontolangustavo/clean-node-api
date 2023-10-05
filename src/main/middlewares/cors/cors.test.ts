a176d1958ca704693f746f70936f4bdf51e51695
Fontolan <fontolangustavo@gmail.com>
2023-10-05 18:55:43 -0300

    feat: ensure cors is enabled

---
+++ src/main/middlewares/cors/cors.test.ts
@@ -0,0 +1,14 @@
+import * as request from 'supertest'
+import app from '../../config/app'
+
+describe('CORS Middleware', () => {
+  test('should enable cors', async () => {
+    app.get('/test-cors', (req: any, res: any) => {
+      res.send()
+    })
+
+    await request(app)
+      .get('/test-cors')
+      .expect('access-control-allow-origin', '*')
+  });
+});
