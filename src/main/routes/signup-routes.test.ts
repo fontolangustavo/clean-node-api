b1d139271757130f597ec14cce2fc45e212ccb32
Fontolan <fontolangustavo@gmail.com>
2023-10-05 19:25:54 -0300

    feat: ensure SignUp route returns an account on success

---
+++ src/main/routes/signup-routes.test.ts
@@ -0,0 +1,16 @@
+import * as request from 'supertest'
+import app from '../config/app'
+
+describe('SignUp Routes', () => {
+  test('should return an account on success', async () => {
+    await request(app)
+      .post('/api/v1/signup')
+      .send({
+        name: 'any_name',
+        email: 'any_email',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      })
+      .expect(200)
+  });
+});
