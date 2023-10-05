f2f8b63419e871ad671ad0daf4b961f5d9aca1d9
Fontolan <fontolangustavo@gmail.com>
2023-10-05 19:03:06 -0300

    feat: ensure return json as default content type

---
+++ src/main/middlewares/content-type/content-type.test.ts
@@ -0,0 +1,25 @@
+import * as request from 'supertest'
+import app from '../../config/app'
+
+describe('Content Type Middleware', () => {
+  test('should return default content type as json', async () => {
+    app.get('/test-content-type/json', (req: any, res: any) => {
+      res.send()
+    })
+
+    await request(app)
+      .get('/test-content-type/json')
+      .expect('content-type', /json/)
+  });
+
+  test('should return xml content type when forced', async () => {
+    app.get('/test-content-type/xml', (req: any, res: any) => {
+      res.type('xml')
+      res.send()
+    })
+
+    await request(app)
+      .get('/test-content-type/xml')
+      .expect('content-type', /xml/)
+  });
+});
