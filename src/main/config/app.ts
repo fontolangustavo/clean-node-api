0107a531ea31c5c3b2e092bf0fa9cd83bc872389
Fontolan <fontolangustavo@gmail.com>
2023-10-05 22:02:53 -0300

    feat: connect to mongodb before app listen

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -2,7 +2,7 @@ import * as express from 'express';
 import setupMiddleware from './middlewares'
 import setupRoutes from './routes'
 
-const app = express()
+const app = (express as any).default();
 setupMiddleware(app)
 setupRoutes(app)
 
