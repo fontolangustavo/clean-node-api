31f7240e158a5a1efb19b2bc1e729cdb6439fb4e
Fontolan <fontolangustavo@gmail.com>
2023-10-06 16:15:58 -0300

    feat: create log factory

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,15 +1,18 @@
+import { Controller, HttpRequest, HttpResponse } from "@/presentation/protocols";
 import { DbAddAccount } from "../../data/usecases/add-account/db-add-account";
 import { BcryptAdapter } from "../../infra/criptography/bcrypt-adapter";
 import { AccountMongoRepository } from "../../infra/db/mongodb/account-repository/account";
 import { SignUpController } from "../../presentation/controllers/signup/signup";
 import { EmailValidatorAdapter } from "../../utils/email-validator";
+import { LogControllerDecorator } from "../decorations/log";
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
   const salt = 12
   const bcryptAdapter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
   const emailValidatorAdapter = new EmailValidatorAdapter()
+  const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount)
 
-  return new SignUpController(emailValidatorAdapter, dbAddAccount)
+  return new LogControllerDecorator(signUpController)
 }
