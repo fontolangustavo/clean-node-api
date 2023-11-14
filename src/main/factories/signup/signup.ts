d98d7caf2865567e9786642e3affd17abf85fd98
Fontolan <fontolangustavo@gmail.com>
2023-11-14 18:03:19 -0300

    refactor: move bcrypt-adapter files to a folder

--- src/main/factories/signup/signup.ts
+++ src/main/factories/signup/signup.ts
@@ -1,6 +1,6 @@
 import { Controller } from "@/presentation/protocols";
 import { DbAddAccount } from "../../../data/usecases/add-account/db-add-account";
-import { BcryptAdapter } from "../../../infra/criptography/bcrypt-adapter";
+import { BcryptAdapter } from "../../../infra/criptography/bcrypt-adapter/bcrypt-adapter";
 import { AccountMongoRepository } from "../../../infra/db/mongodb/account-repository/account";
 import { LogMongoRespository } from "../../../infra/db/mongodb/log-repository/log";
 import { SignUpController } from "../../../presentation/controllers/signup/signup";
