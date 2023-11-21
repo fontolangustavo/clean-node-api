582b2d1730f59753932f25d1107d6e25d803d8e9
Fontolan <fontolangustavo@gmail.com>
2023-11-21 12:52:17 -0300

    chore: add engines with node version

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://fontolangustavo:ABoXjxJlaIDiiVGw@example-node-api.rm8e6f0.mongodb.net/?retryWrites=true&w=majority',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || 'hashcoo-touneslo-toash',
 }
