1bc04a146ed2e6134cb2ee97b7297fda3fb96185
Fontolan <fontolangustavo@gmail.com>
2023-11-21 17:54:08 -0300

    chore: add docker-compose

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://fontolangustavo:ABoXjxJlaIDiiVGw@example-node-api.rm8e6f0.mongodb.net/?retryWrites=true&w=majority',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/clean-node-api',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || 'hashcoo-touneslo-toash',
 }
