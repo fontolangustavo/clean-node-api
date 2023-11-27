41959512aaaa3d3542af6ad532ae99847b098bb8
Fontolan <fontolangustavo@gmail.com>
2023-11-27 14:01:51 -0300

    test: ensure add survey route returns 204 if valid access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -1,9 +1,12 @@
 import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper';
-import { Collection } from 'mongodb';
+import { Collection, ObjectId } from 'mongodb';
+import { sign } from 'jsonwebtoken';
+import env from '../config/env';
 
 let surveyCollection: Collection
+let accountCollection: Collection
 
 describe('Surey Routes', () => {
 
@@ -18,10 +21,13 @@ describe('Surey Routes', () => {
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.deleteMany({})
+
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
   })
 
   describe('POST /surveys', () => {
-    test('should return 403 on add without accessToken', async () => {
+    test('should return 403 on add survey without accessToken', async () => {
       await request(app)
         .post('/api/v1/surveys')
         .send({
@@ -37,6 +43,43 @@ describe('Surey Routes', () => {
           ]
         })
         .expect(403)
+    });
+
+    test('should return 204 on add survey with valid accessToken', async () => {
+      const res = await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@gmail.com',
+        password: 'any_password',
+        role: 'admin'
+      })
+
+      const id = res.insertedId.toString()
+      const accessToken = sign({id}, env.jwtSecret)
+
+      await accountCollection.updateOne({
+        _id: new ObjectId(id)
+      }, {
+        $set: {
+          accessToken
+        }
+      })
+
+      await request(app)
+        .post('/api/v1/surveys')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'any_question',
+          answers: [
+            {
+              image: 'any_image',
+              answer: 'any_answer'
+            },
+            {
+              answer: 'any_answer_2'
+            }
+          ]
+        })
+        .expect(204)
     });
   });
 });
