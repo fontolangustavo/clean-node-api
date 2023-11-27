a30997ce69f874c2253bb3e68c20947ed6dafce8
Fontolan <fontolangustavo@gmail.com>
2023-11-27 12:10:55 -0300

    test: ensure AccountMongoRepository will not find an account if find account by token fails

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -123,5 +123,13 @@ describe('Account Mongo Repository', () => {
       expect(account.email).toBe('any_email@email.com')
       expect(account.password).toBe('any_password')
     })
+
+    test('should return null if loadByToken fails', async () => {
+      const sut = makeSut()
+
+      const account = await sut.loadByToken('any_token')
+
+      expect(account).toBeFalsy()
+    })
   });
 })
