59e0fc3a96b0fe505631d6a514caac72cd8517cc
Fontolan <fontolangustavo@gmail.com>
2023-11-30 17:52:04 -0300

    feat: ensure DbLoadSurveys calls LoadSurveysRepository

---
+++ src/data/usecases/load-surveys/db-load-surveys.spec.ts
@@ -0,0 +1,75 @@
+import MockDate from 'mockdate'
+
+import { LoadSurveysRepository, SurveyModel } from './db-load-surveys.protocols'
+import { DbLoadSurveys } from './db-load-surveys'
+
+const makeFakeSurveys = (): SurveyModel[] => {
+  return [
+    {
+      id: 'any_id',
+      question: 'any_questions',
+      created_at: new Date(),
+      answers: [
+        {
+          image: 'any_image',
+          answer: 'any_answer'
+        },
+      ],
+    },
+    {
+      id: 'any_id',
+      question: 'other_questions',
+      created_at: new Date(),
+      answers: [
+        {
+          image: 'other_image',
+          answer: 'other_answer'
+        },
+      ],
+    }
+  ]
+}
+
+const makeLoadSurveysRepository = (): LoadSurveysRepository => {
+  class LoadSurveysRepositoryStub implements LoadSurveysRepository {
+    async load(): Promise<SurveyModel[]> {
+      return Promise.resolve(makeFakeSurveys())
+    }
+  }
+
+  return new LoadSurveysRepositoryStub()
+}
+
+interface SutTypes {
+  loadSurveysRepositoryStub: LoadSurveysRepository
+  sut: DbLoadSurveys
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveysRepositoryStub = makeLoadSurveysRepository()
+  const sut = new DbLoadSurveys(loadSurveysRepositoryStub)
+
+  return {
+    sut,
+    loadSurveysRepositoryStub
+  }
+}
+
+describe('DbLoadSurveys Usecase', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
+  test('should call LoadSurveyRepository', async () => {
+    const { sut, loadSurveysRepositoryStub } = makeSut()
+    const loadSpy = jest.spyOn(loadSurveysRepositoryStub, 'load')
+
+    await sut.load()
+
+    expect(loadSpy).toHaveBeenCalled()
+  });
+});
