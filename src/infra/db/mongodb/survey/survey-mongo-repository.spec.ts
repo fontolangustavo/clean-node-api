ffc5036a274a80675fb34ef6031d5a97378c29b7
Fontolan <fontolangustavo@gmail.com>
2023-12-02 07:12:56 -0300

    feat: ensure SurveyMongoRepository load all surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -45,4 +45,36 @@ describe('Survey Mongo Repository', () => {
     })
 
   });
+
+  describe('load()', () => {
+    test('should load surveys on success', async () => {
+      const sut = makeSut()
+      await surveyCollection.insertMany([
+        {
+          question: 'any_question',
+          created_at: new Date(),
+          answers: [
+            {
+              answer: 'any_answer'
+            },
+          ]
+        },
+        {
+          question: 'other_question',
+          created_at: new Date(),
+          answers: [
+            {
+              answer: 'other_answer'
+            },
+          ]
+        }])
+
+      const surveys = await sut.load()
+
+      expect(surveys.length).toBe(2)
+      expect(surveys[0].question).toBe('any_question')
+      expect(surveys[1].question).toBe('other_question')
+    })
+
+  });
 })
--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,11 +1,21 @@
+import { LoadSurveysRepository, SurveyModel } from '@/data/usecases/load-surveys/db-load-surveys.protocols'
 import { AddSurveyRepository } from '../../../../data/protocols/db/survey/add-survey-repository'
 import { AddSurveyModel } from '../../../../domain/usecases/add-survey'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class SurveyMongoRepository implements AddSurveyRepository {
+export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
   async add(data: AddSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
 
     await surveyCollection.insertOne(data)
+  }
+
+  async load(): Promise<SurveyModel[]> {
+    const surveyCollection = await MongoHelper.getCollection('surveys')
+
+    /* @ts-ignore */
+    const surveys: SurveyModel[] = await surveyCollection.find().toArray()
+
+    return surveys
   }
 }
