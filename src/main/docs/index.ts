70f2784dd60207cbaa079d4804bf5850a6fc9210
Fontolan <fontolangustavo@gmail.com>
2024-01-03 09:55:28 -0300

    docs: add login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,29 @@
+import { loginPath } from './paths/login-path'
+import { accountSchema } from './schemas/account-schema'
+import { loginParamsSchema } from './schemas/login-params-schema'
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Clean Node API',
     description: 'API do curso do Mango de enquetes',
     version: '1.0.0'
+  },
+  servers: [
+    {
+      url: '/api/v1'
+    }
+  ],
+  tags: [
+    {
+      name: 'Login'
+    }
+  ],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema
   }
 }
---
+++ src/main/docs/paths/login-path.ts
@@ -0,0 +1,29 @@
+export const loginPath = {
+  post: {
+    tags: [
+      'Login'
+    ],
+    summary: 'API para autenticar usuario',
+    requestBody: {
+      content: {
+        'application/json': {
+          schema: {
+            $ref: '#/schemas/loginParams'
+          }
+        }
+      }
+    },
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/account'
+            }
+          }
+        }
+      }
+    }
+  }
+}
