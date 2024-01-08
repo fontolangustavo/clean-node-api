97acaa77ee6127e0702fe5c91ba5deaf37ebd749
Fontolan <fontolangustavo@gmail.com>
2024-01-08 11:37:29 -0300

    refactor: move LoadSurveyResult dependencies to a protocols file

--- src/data/usecases/survey-result/load-survey-result/db-load-survey-result.spec.ts
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.spec.ts
@@ -1,6 +1,6 @@
-import { LoadSurveyResultRepository } from '@/data/protocols/db/survey-result/load-survey-result-repository'
 import { DbLoadSurveyResult } from "./db-load-survey-result";
 import { mockLoadSurveyResultRepositoryStub } from "@/data/test";
+import { LoadSurveyResultRepository } from './db-load-survey-result.protocols'
 
 type SutType = {
   sut: DbLoadSurveyResult
