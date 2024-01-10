3b9ca2c5cdc611ae05d60f1f2bd96ad0d198aade
Fontolan <fontolangustavo@gmail.com>
2024-01-10 11:44:06 -0300

    feat: ensure LoadSurveyResultController returns 500 if LoadSurveyById throws

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
@@ -1,6 +1,8 @@
 import { mockLoadSurveyById } from "@/presentation/test";
 import { LoadSurveyResultController } from "./load-survey-result-controller";
 import { HttpRequest, LoadSurveyById } from "./load-survey-result-controller-protocols";
+import { forbidden, serverError } from "@/presentation/helpers/http/http-helper";
+import { InvalidParamError } from "@/presentation/errors";
 
 
 const makeFakeRequest = (): HttpRequest => ({
@@ -39,5 +41,25 @@ describe('LoadSurveyResult Controller', () => {
     await sut.handle(makeFakeRequest())
 
     expect(loadSurveyByIdSpy).toHaveBeenCalledWith('any_id')
+  });
+
+  test('should return 403 if LoadSurveyById returns null', async () => {
+    const { sut, loadSurveyByIdStub } = makeSut()
+
+    jest.spyOn(loadSurveyByIdStub, 'loadById').mockReturnValueOnce(Promise.resolve(null))
+
+    const httpResponse = await sut.handle(makeFakeRequest())
+
+    expect(httpResponse).toEqual(forbidden(new InvalidParamError('surveyId')))
+  });
+
+  test('should return 500 if LoadSurveyById throws', async () => {
+    const { sut, loadSurveyByIdStub } = makeSut()
+
+    jest.spyOn(loadSurveyByIdStub, 'loadById').mockImplementation(() => { throw new Error() })
+
+    const httpResponse = await sut.handle(makeFakeRequest())
+
+    expect(httpResponse).toEqual(serverError(new Error()))
   });
 });
--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
@@ -4,6 +4,8 @@ import {
   HttpResponse,
   LoadSurveyById
 } from "./load-survey-result-controller-protocols";
+import { InvalidParamError } from "@/presentation/errors";
+import { forbidden, serverError } from "@/presentation/helpers/http/http-helper";
 
 export class LoadSurveyResultController implements Controller {
   constructor(
@@ -11,10 +13,18 @@ export class LoadSurveyResultController implements Controller {
   ) { }
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { surveyId } = httpRequest.params
+    try {
+      const { surveyId } = httpRequest.params
 
-    await this.loadSurveyById.loadById(surveyId)
+      const survey = await this.loadSurveyById.loadById(surveyId)
 
-    return null
+      if (!survey) {
+        return forbidden(new InvalidParamError('surveyId'))
+      }
+
+      return null
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
