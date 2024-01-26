3756a2cb8e99a604bbf83774484007ad1978d4bd
Fontolan <fontolangustavo@gmail.com>
2024-01-26 18:42:57 -0300

    test: ensure SignUp Mutation returns correct values

--- src/main/graphql/tests/login.tests.ts
+++ src/main/graphql/tests/login.tests.ts
@@ -26,6 +26,7 @@ describe('Login GraphQL', () => {
     accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
+
   describe('Login Query', () => {
     const loginQuery = gql`
       query login($email: String!, $password: String!) {
@@ -75,6 +76,60 @@ describe('Login GraphQL', () => {
       expect(response.data).toBeFalsy()
       expect(response.errors[0].message).toBe
         ('Unauthorized')
+    })
+  });
+
+  describe('SignUp Mutation', () => {
+    const signUpMutation = gql`
+      mutation signUp($name: String!, $email: String!, $password: String!, $passwordConfirmation: String!) {
+        signUp(name: $name, email: $email, password: $password, passwordConfirmation: $passwordConfirmation) {
+          accessToken
+          name
+        }
+      }
+    `
+
+    test('should return an Account on valid data', async () => {
+      const { mutate } = createTestClient({
+        apolloServer
+      })
+
+      const response: any = await mutate(signUpMutation, {
+        variables: {
+          name: 'any_name',
+          email: 'any_email@gmail.com',
+          password: 'any_password',
+          passwordConfirmation: 'any_password'
+        }
+      })
+
+      expect(response.data.signUp.accessToken).toBeTruthy()
+      expect(response.data.signUp.name).toBe
+        ('any_name')
+    })
+
+    test('should return EmailInUseError on invalid data', async () => {
+      const password = await hash('any_password', 12)
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@gmail.com',
+        password
+      })
+
+      const { mutate } = createTestClient({
+        apolloServer
+      })
+
+      const response: any = await mutate(signUpMutation, {
+        variables: {
+          email: 'any_email@gmail.com',
+          password: 'any_password'
+        }
+      })
+
+      expect(response.data).toBeFalsy()
+      expect(response.errors[0].message).toBe
+        ('The received email is already in use')
     })
   });
 });
